3fa68e1c677bd1802b315ea97b077000b4558bc9
Exouth <Exouth@protonmail.com>
2024-11-09 20:03:40 +0100

    Improve Logger for Debug and Release logging levels

--- src/SoundFX/Logger.cpp
+++ src/SoundFX/Logger.cpp
@@ -1,10 +1,10 @@
 #include "Logger.h"
+#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
 
 #ifdef NDEBUG
-#    include <spdlog/sinks/basic_file_sink.h>
+// File_Sink
 #else
-#    include <spdlog/sinks/basic_file_sink.h>
 #    include <spdlog/sinks/msvc_sink.h>
 #endif
 
@@ -14,21 +14,22 @@ void
     if (!path) {
         throw std::runtime_error("Log directory could not be found.");
     }
-
     *path /= "SoundFXFramework.log";
+
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 
 #ifdef NDEBUG
     auto logger = std::make_shared<spdlog::logger>("global", std::move(file_sink));
     logger->set_level(spdlog::level::info);
+    logger->flush_on(spdlog::level::warn);
 #else
     auto msvc_sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
     auto logger =
         std::make_shared<spdlog::logger>("global", spdlog::sinks_init_list {msvc_sink, file_sink});
-    logger->set_level(spdlog::level::debug);
+    logger->set_level(spdlog::level::trace);
+    logger->flush_on(spdlog::level::trace);
 #endif
 
-    logger->flush_on(spdlog::level::info);
     spdlog::set_default_logger(std::move(logger));
     spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
     spdlog::info("Logger initialized");
@@ -43,7 +44,7 @@ std::optional<std::filesystem::path>
     SoundFX::Logger::GetLogDirectory() {
     auto path = SKSE::log::log_directory();
     if (!path) {
-        SKSE::stl::report_and_fail("Log directory could not be found.");
+        throw std::runtime_error("Log directory could not be found.");
     }
     return path;
 }
