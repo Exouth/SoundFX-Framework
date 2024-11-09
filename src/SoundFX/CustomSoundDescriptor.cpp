8aa30495d9591a32feabfe27731cddf0d9b05e98
Exouth <Exouth@protonmail.com>
2024-11-09 15:00:30 +0100

    Add CustomSoundDescriptor class for handling and playing custom sound files

---
+++ src/SoundFX/CustomSoundDescriptor.cpp
@@ -0,0 +1,115 @@
+#include "CustomSoundDescriptor.h"
+
+RE::BSISoundDescriptor::BSIPlaybackCharacteristics::~BSIPlaybackCharacteristics() = default;
+RE::BSISoundDescriptor::~BSISoundDescriptor()                                     = default;
+
+namespace SoundFX {
+    CustomSoundDescriptor::CustomSoundDescriptor(const std::string &filePath) {
+        try {
+            RE::BSAudioManager *audioManager = RE::BSAudioManager::GetSingleton();
+            if (audioManager) {
+                RE::BSResource::ID fileID;
+                fileID.GenerateFromPath(filePath.c_str());
+                audioManager->BuildSoundDataFromFile(
+                    soundHandle, fileID, /*flags=*/0x1A, /*priority=*/1);
+            } else {
+                spdlog::error("AudioManager is null.");
+            }
+        } catch (const std::exception &e) {
+            spdlog::error("Error creating SoundDescriptor: {}", e.what());
+        }
+    }
+
+    bool
+        CustomSoundDescriptor::Play() {
+        if (soundHandle.IsValid()) {
+            return soundHandle.Play();
+        }
+        spdlog::warn("SoundHandle is invalid.");
+        return false;
+    }
+
+    void
+        CustomSoundDescriptor::Stop() {
+        if (soundHandle.IsValid()) {
+            soundHandle.Stop();
+        }
+    }
+
+    RE::BSSoundHandle &
+        CustomSoundDescriptor::GetSoundHandle() {
+        return soundHandle;
+    }
+
+    bool
+        CustomSoundDescriptor::IsPlaying() const {
+        return soundHandle.IsPlaying();
+    }
+
+    bool
+        CustomSoundDescriptor::FadeInPlay(std::uint16_t fadeTimeMS) {
+        if (soundHandle.IsValid()) {
+            return soundHandle.FadeInPlay(fadeTimeMS);
+        }
+        return false;
+    }
+
+    bool
+        CustomSoundDescriptor::FadeOutAndRelease(std::uint16_t fadeTimeMS) {
+        if (soundHandle.IsValid()) {
+            return soundHandle.FadeOutAndRelease(fadeTimeMS);
+        }
+        return false;
+    }
+
+    bool
+        CustomSoundDescriptor::SetVolume(float volume) {
+        if (soundHandle.IsValid()) {
+            return soundHandle.SetVolume(volume);
+        }
+        return false;
+    }
+
+    bool
+        CustomSoundDescriptor::SetPosition(RE::NiPoint3 position) {
+        if (soundHandle.IsValid()) {
+            return soundHandle.SetPosition(position);
+        }
+        return false;
+    }
+
+    CustomSoundDescriptor::CustomPlaybackCharacteristics &
+        CustomSoundDescriptor::GetPlaybackCharacteristics() {
+        return playbackCharacteristics;
+    }
+
+    void
+        CustomSoundDescriptor::Unk_01() {
+    }
+
+    void
+        CustomSoundDescriptor::Unk_02() {
+    }
+
+    std::uint8_t
+        CustomSoundDescriptor::CustomPlaybackCharacteristics::GetFrequencyShift() {
+        return 0;
+    }
+    std::uint8_t
+        CustomSoundDescriptor::CustomPlaybackCharacteristics::GetFrequencyVariance() {
+        return 0;
+    }
+    std::uint8_t
+        CustomSoundDescriptor::CustomPlaybackCharacteristics::GetPriority() {
+        return 1;
+    }
+    std::uint16_t
+        CustomSoundDescriptor::CustomPlaybackCharacteristics::GetStaticAttenuation() {
+        return 0;
+    }
+    std::uint8_t
+        CustomSoundDescriptor::CustomPlaybackCharacteristics::GetDBVariance() {
+        return 0;
+    }
+
+}
