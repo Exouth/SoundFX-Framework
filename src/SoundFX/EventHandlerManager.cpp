49fa1175ff59a5bfe5b5658e2c48d20ade4421e4
Exouth <Exouth@protonmail.com>
2024-11-09 21:11:40 +0100

    Add base EventHandlerManager with initial EquipEvent in WeaponEventHandler for testing

---
+++ src/SoundFX/EventHandlerManager.cpp
@@ -0,0 +1,27 @@
+#include "EventHandlerManager.h"
+#include "Logger.h"
+#include <RE/Skyrim.h>
+
+namespace SoundFX {
+
+    EventHandlerManager::EventHandlerManager(JSONLoader &jsonLoader) : jsonLoader(jsonLoader) {
+    }
+
+    void
+        EventHandlerManager::InitializeEventHandlers() {
+        auto eventSource = RE::ScriptEventSourceHolder::GetSingleton();
+
+        weaponEventHandler = std::make_unique<WeaponEventHandler>(jsonLoader);
+        RegisterEventHandler<RE::TESEquipEvent>(eventSource, weaponEventHandler.get());
+        RegisterEventHandler<RE::TESActivateEvent>(eventSource, weaponEventHandler.get());
+    }
+
+    template <typename EventType, typename HandlerType>
+    void
+        EventHandlerManager::RegisterEventHandler(RE::ScriptEventSourceHolder *eventSource,
+                                                  HandlerType                 *handler) {
+        eventSource->AddEventSink<EventType>(handler);
+        spdlog::info("Registered handler for event type: {}", typeid(EventType).name());
+    }
+
+}
---
+++ src/SoundFX/EventHandlers/WeaponEventHandler.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "RE/B/BSTEvent.h"
+#include "JSONLoader.h"
+
+namespace SoundFX {
+
+    class WeaponEventHandler :
+        public RE::BSTEventSink<RE::TESActivateEvent>,
+        public RE::BSTEventSink<RE::TESEquipEvent>,
+        public RE::BSTEventSink<RE::TESHitEvent> {
+        JSONLoader &jsonLoader;
+
+      public:
+        explicit WeaponEventHandler(JSONLoader &loader) : jsonLoader(loader) {
+        }
+
+        RE::BSEventNotifyControl
+            ProcessEvent(const RE::TESActivateEvent *event,
+                         RE::BSTEventSource<RE::TESActivateEvent> *);
+        RE::BSEventNotifyControl
+            ProcessEvent(const RE::TESEquipEvent *event, RE::BSTEventSource<RE::TESEquipEvent> *);
+        RE::BSEventNotifyControl
+            ProcessEvent(const RE::TESHitEvent *event, RE::BSTEventSource<RE::TESHitEvent> *);
+
+      private:
+        RE::BSEventNotifyControl
+            ProcessActivateEvent(const RE::TESActivateEvent *event);
+        RE::BSEventNotifyControl
+            ProcessEquipEvent(const RE::TESEquipEvent *event);
+        RE::BSEventNotifyControl
+            ProcessHitEvent(const RE::TESHitEvent *event);
+    };
+
+}
