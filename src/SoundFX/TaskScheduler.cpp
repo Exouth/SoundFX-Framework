89f9f69b31e58af94d005ad641a9c9db0c09d397
Exouth <Exouth@protonmail.com>
2024-11-12 21:33:54 +0100

    Refactor TaskScheduler to use std::async instead of SKSE Tasks preventing delays and improving timing (Decreasing IntervalMs for TaskScheduler)

--- src/SoundFX/TaskScheduler.cpp
+++ src/SoundFX/TaskScheduler.cpp
@@ -1,50 +1,61 @@
 #include "TaskScheduler.h"
-#include "SKSE/API.h"
 
-TaskScheduler::TaskScheduler() : stopFlag(false) {
-}
+namespace SoundFX {
 
-TaskScheduler::~TaskScheduler() {
-    Stop();
-}
-
-void
-    TaskScheduler::Start(int intervalMilliseconds) {
-    stopFlag        = false;
-    schedulerThread = std::thread([this, intervalMilliseconds]() {
-        while (!stopFlag) {
-            auto start = std::chrono::steady_clock::now();
-
-            std::lock_guard<std::mutex> lock(taskMutex);
-            for (auto &[task, repeat] : tasks) {
-                SKSE::GetTaskInterface()->AddTask([task]() { std::thread(task).detach(); });
-
-                if (!repeat) {
-                    task = nullptr;
-                }
-            }
-            tasks.erase(std::remove_if(tasks.begin(),
-                                       tasks.end(),
-                                       [](const auto &t) { return t.first == nullptr; }),
-                        tasks.end());
-
-            auto end = std::chrono::steady_clock::now();
-            std::this_thread::sleep_for(std::chrono::milliseconds(intervalMilliseconds)
-                                        - (end - start));
-        }
-    });
-}
-
-void
-    TaskScheduler::Stop() {
-    stopFlag = true;
-    if (schedulerThread.joinable()) {
-        schedulerThread.join();
+    TaskScheduler::TaskScheduler() : stopFlag(false) {
     }
-}
 
-void
-    TaskScheduler::AddTask(Task task, bool repeat) {
-    std::lock_guard<std::mutex> lock(taskMutex);
-    tasks.emplace_back(std::move(task), repeat);
+    TaskScheduler::~TaskScheduler() {
+        Stop();
+    }
+
+    void
+        TaskScheduler::Start(int intervalMilliseconds) {
+        stopFlag        = false;
+        schedulerThread = std::thread([this, intervalMilliseconds]() {
+            while (!stopFlag) {
+                auto start = std::chrono::steady_clock::now();
+
+                std::lock_guard<std::mutex> lock(taskMutex);
+                for (auto &[task, repeat] : tasks) {
+                    // SKSE::GetTaskInterface()->AddTask([task]() { std::thread(task).detach(); });
+
+                    // Tasks were sometimes executed consecutively within the same interval, causing
+                    // a delay. By using std::async, each task runs independently
+                    std::async(std::launch::async, [task, this]() {
+
+                        if (!stopFlag) {
+                            task();
+                        }
+                    });
+
+                    if (!repeat) {
+                        task = nullptr;
+                    }
+                }
+                tasks.erase(std::remove_if(tasks.begin(),
+                                           tasks.end(),
+                                           [](const auto &t) { return t.first == nullptr; }),
+                            tasks.end());
+
+                auto end = std::chrono::steady_clock::now();
+                std::this_thread::sleep_for(std::chrono::milliseconds(intervalMilliseconds)
+                                            - (end - start));
+            }
+        });
+    }
+
+    void
+        TaskScheduler::Stop() {
+        stopFlag = true;
+        if (schedulerThread.joinable()) {
+            schedulerThread.join();
+        }
+    }
+
+    void
+        TaskScheduler::AddTask(Task task, bool repeat) {
+        std::lock_guard<std::mutex> lock(taskMutex);
+        tasks.emplace_back(std::move(task), repeat);
+    }
 }
--- src/SoundFX/TaskScheduler.h
+++ src/SoundFX/TaskScheduler.h
@@ -1,28 +1,31 @@
 #pragma once
 
-/// A lightweight task scheduler to perform tasks periodically or repeatedly.
-/// This scheduler has been performance-tested on an Intel i9-14900K processor, managing 50
-/// tasks at the same time without consuming any noticeable CPU or RAM. FPS
-/// performance was unaffected even when running with 50 concurrent threads every second
-class TaskScheduler {
-  public:
-    using Task = std::function<void()>;
+namespace SoundFX {
 
-    TaskScheduler();
-    ~TaskScheduler();
+    /// A lightweight task scheduler to perform tasks periodically or repeatedly.
+    /// This scheduler has been performance-tested on an Intel i9-14900K processor, managing 50
+    /// tasks at the same time without consuming any noticeable CPU or RAM. FPS
+    /// performance was unaffected even when running with 50 concurrent threads every second
+    class TaskScheduler {
+      public:
+        using Task = std::function<void()>;
 
-    void
-        Start(int intervalMilliseconds = 1000);
+        TaskScheduler();
+        ~TaskScheduler();
 
-    void
-        Stop();
+        void
+            Start(int intervalMilliseconds = 500);
 
-    void
-        AddTask(Task task, bool repeat = false);
+        void
+            Stop();
 
-  private:
-    std::atomic<bool>                  stopFlag;
-    std::thread                        schedulerThread;
-    std::vector<std::pair<Task, bool>> tasks;
-    std::mutex                         taskMutex;
-};
+        void
+            AddTask(Task task, bool repeat = false);
+
+      private:
+        std::atomic<bool>                  stopFlag;
+        std::thread                        schedulerThread;
+        std::vector<std::pair<Task, bool>> tasks;
+        std::mutex                         taskMutex;
+    };
+}
