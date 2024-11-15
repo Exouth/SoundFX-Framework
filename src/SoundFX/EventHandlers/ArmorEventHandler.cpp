bd3c88cc5262a41c2281202131dde3bd4a6f919c
Exouth <Exouth@protonmail.com>
2024-11-15 20:57:50 +0100

    Added PickUpEvent for Armor

--- src/SoundFX/EventHandlers/ArmorEventHandler.cpp
+++ src/SoundFX/EventHandlers/ArmorEventHandler.cpp
@@ -15,6 +15,35 @@ namespace SoundFX {
     RE::BSEventNotifyControl
         ArmorEventHandler::ProcessPickUpEvent(const RE::TESContainerChangedEvent *event) {
 
+        if (event == nullptr || event->newContainer == 0) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        const RE::TESForm *item = RE::TESForm::LookupByID(event->baseObj);
+        if (!item) {
+            spdlog::warn("Failed to lookup item for PickUpEvent.");
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        const auto &weapons = jsonLoader.getItems("armors");
+        for (const auto &[itemName, itemEvents] : weapons) {
+            const auto resolvedFormID =
+                GetFormIDFromEditorIDAndPluginName(itemEvents.editorID, itemEvents.pluginName);
+
+            if (resolvedFormID == item->formID) {
+                for (const auto &jsonEvent : itemEvents.events) {
+                    if (jsonEvent.type == "PickUp") {
+                        float randomValue = static_cast<float>(rand()) / RAND_MAX;
+                        if (randomValue <= jsonEvent.chance) {
+                            spdlog::info("Playing PickUp sound for weapon: {}",
+                                         jsonEvent.soundEffect);
+                            PlayCustomSoundAsDescriptor(jsonEvent.soundEffect);
+                        }
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                }
+            }
+        }
         return RE::BSEventNotifyControl::kContinue;
     }
 }
