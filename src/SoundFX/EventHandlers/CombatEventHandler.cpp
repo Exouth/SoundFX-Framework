4a349c4957f3a37bead646a74d009a216b5f34d0
Exouth <Exouth@protonmail.com>
2025-02-19 16:13:34 +0100

    Added StartCombatEvent for CombatEvents

--- src/SoundFX/EventHandlers/CombatEventHandler.cpp
+++ src/SoundFX/EventHandlers/CombatEventHandler.cpp
@@ -15,6 +15,33 @@ namespace SoundFX {
     RE::BSEventNotifyControl
         CombatEventHandler::ProcessStartCombatEvent(const RE::TESCombatEvent *event) {
 
+        if (event == nullptr || event->newState.any(RE::ACTOR_COMBAT_STATE::kNone)) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        if (!event->targetActor || !event->actor || !event->actor->GetBaseObject()) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        const auto &combats = jsonLoader.getItems("combats");
+        for (const auto &[combatName, combatEvents] : combats) {
+            const auto resolvedFormID =
+                GetFormIDFromEditorIDAndPluginName(combatEvents.editorID, combatEvents.pluginName);
+
+            if (resolvedFormID == event->actor->GetBaseObject()->formID) {
+                for (const auto &jsonEvent : combatEvents.events) {
+                    if (jsonEvent.type == "Start"
+                        && event->newState.any(RE::ACTOR_COMBAT_STATE::kCombat)) {
+                        float randomValue = static_cast<float>(rand()) / RAND_MAX;
+                        if (randomValue <= jsonEvent.chance) {
+                            PlayCustomSoundAsDescriptor(jsonEvent.soundEffect);
+                        }
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                }
+            }
+        }
+
         return RE::BSEventNotifyControl::kContinue;
     }
 }
